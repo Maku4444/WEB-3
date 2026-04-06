fae2fb579cb6291dc24739b4004db3dbb0bfa645
Maku4444 <dzulaymakar@gmail.com>
2026-04-06 09:59:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Налаштування для збереження фото (Варіант 5: Фото)
+
 const storage = multer.diskStorage({
     destination: './uploads/',
     filename: (req, file, cb) => {
@@ -15,16 +15,16 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 app.use(express.json());
-app.use(express.static('public')); // Сервер роздає твій HTML з папки public
-app.use('/uploads', express.static('uploads')); // Роздає завантажені фото
+app.use(express.static('public')); 
+app.use('/uploads', express.static('uploads')); 
 
-// Маршрут для завантаження списку аварій
+
 app.get('/api/accidents', (req, res) => {
     const data = JSON.parse(fs.readFileSync('data.json', 'utf8') || "[]");
     res.json(data);
 });
 
-// Маршрут для реєстрації нової аварії (Варіант 5: Дата, Тип, Адреса, Опис, Постраждалі, Час)
+
 app.post('/api/accidents', upload.single('photo'), (req, res) => {
     const accidents = JSON.parse(fs.readFileSync('data.json', 'utf8') || "[]");
     
